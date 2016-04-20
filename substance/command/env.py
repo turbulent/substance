236c836b4d09c12684da949bd9f975af764dea64
Benoit Beausejour <b@turbulent.ca>
2016-04-20 11:05:23 -0400

    Env command

---
+++ substance/command/env.py
@@ -0,0 +1,28 @@
+from substance.monads import *
+from substance.logs import *
+from substance.engine import Engine
+from substance.command import Command
+from substance.exceptions import (SubstanceError)
+
+class Env(Command):
+
+  def getShellOptions(self, optparser):
+    return optparser
+
+  def main(self):
+
+    name = self.getInputName()
+
+    self.core.initialize() \
+      .then(defer(self.core.loadEngine, name)) \
+      .bind(Engine.loadConfigFile) \
+      .bind(self.outputDockerEnv) \
+      .catch(self.exitError)
+
+  def outputDockerEnv(self, engine):
+    publicIP = engine.getPublicIP()
+    port = engine.getDockerPort()
+    print("export DOCKER_API_VERSION=\"1.19\"")   
+    print("export DOCKER_HOST=\"%s\"" % (engine.getDockerURL()))
+    print("export DOCKER_TLS_VERIFY=\"\"")
+    return OK(None)
