f23694441e59cbacdf4a694b72948a0df1775d73
Benoit Beausejour <b@turbulent.ca>
2017-11-04 20:16:48 -0400

    Fix improper import order

--- substance/subenv/__init__.py
+++ substance/subenv/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
-from .envspec import SubenvSpec
-from .api import SubenvAPI
-from .cli import SubenvCLI
-
 SPECDIR = '.substance'
 ENVFILE = '.env'
 CODELINK = 'code'
 CONFFILE = 'subenv.yml'
+
+from .envspec import SubenvSpec
+from .api import SubenvAPI
+from .cli import SubenvCLI
